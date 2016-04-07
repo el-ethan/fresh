f4d2004ae363bdd68a65f9d5603ca78b688a5e32
el-ethan <elethan@gmail.com>
2016-04-07 10:36:55 -0500

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # Install ansible
 sudo apt-get update
-sudo apt-get install ansible git
+sudo apt-get -y install ansible git
 
 # Clone provisioning repo
 cd ~ && git clone https://github.com/el-ethan/fresh.git
@@ -10,18 +10,3 @@ cd fresh
 ansible-playbook setup.yml -i HOSTS --ask-sudo-pass
 
 dropbox start -i > /dev/null 2>&1 &
-
-# Emacs configuration
-cd ~ && git clone git@github.com:el-ethan/.emacs.d.git
-
-# Get dot files
-git clone git@github.com:el-ethan/dotfiles.git
-
-# Get Terminator theme
-if [ -d "/home/ethan/.config/terminator" ]; then
-  rm -rf ~/.config/terminator
-fi
-
-cd ~/.config && git clone git@github.com:el-ethan/terminator.git
-
-sudo ./emacs.sh
